cc45e2dc4959b75adc8cd82a422f8e12bd4a47f8
deep-learning-engineer1 <148576909+deep-learning-engineer1@users.noreply.github.com>
2024-07-02 13:06:35 +0300

    model_training.py

--- model_training.py
+++ model_training.py
@@ -1,1 +1,31 @@
+import tensorflow as tf
+import tensorflow.keras.layers
+import tensorflow.keras.models
+
+model = tf.keras.models.Sequential([
+
+  #Block №1
+  conv_layer1 = tf.keras.layers.Conv2D(),
+  concatenate_layer1 = tf.keras.layers.Concatenate(),
+  concatenate_layer1 = conv_layer1,
+  max_pool_layer1 = tf.keras.layers.MaxPooling2D(),
+  max_pool_layer1 = conv_layer1,
+
+
+  #Block №2
+  conv_layer2 = tf.keras.layers.Conv2D(),
+  concatenate_layer2 = tf.keras.layers.Concatenate(),
+  concatenate_layer2 = conv_layer2,
+  max_pool_layer2 = tf.keras.layers.MaxPooling2D(),
+  max_pool_layer2 = conv_layer2,
+
+  #Block №3
+  conv_layer2 = tf.keras.layers.Conv2D(),
+  concatenate_layer2 = tf.keras.layers.Concatenate(),
+  concatenate_layer2 = conv_layer2,
+  max_pool_layer2 = tf.keras.layers.MaxPooling2D(),
+  max_pool_layer2 = conv_layer2,
+])
+
+#tf.keras.layers.Concatenate
 
