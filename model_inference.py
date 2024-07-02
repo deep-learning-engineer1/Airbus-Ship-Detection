c9d8b5f9c6bcdc415ce3e909938918f7c2d6e52f
deep-learning-engineer1 <148576909+deep-learning-engineer1@users.noreply.github.com>
2024-07-02 14:29:56 +0300

    model_inference.py

--- model_inference.py
+++ model_inference.py
@@ -1,1 +1,13 @@
+import tensorflow as tf
+import tensorflow.keras.layers
+import tensorflow.keras.models
 
+
+tf.keras.load_model("./")
+
+def preprocess():
+  pass
+  
+def predict():
+  preprocess()
+  pass
