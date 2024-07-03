c40b8188ae3dc55c8821788b2f59388a6b0b7a90
deep-learning-engineer1 <148576909+deep-learning-engineer1@users.noreply.github.com>
2024-07-03 13:29:26 +0300

    Update model_inference.py

--- model_inference.py
+++ model_inference.py
@@ -3,14 +3,20 @@ import tensorflow.keras.layers
 import tensorflow.keras.models
 
 your_directory = ""
+length = 
+width = 
 
+data = tf.keras.preprocessing.image_dataset_from_directory(
+    directory = your_directory,
+    color_mode = "rgb",
+    image_size = (length, width),
+    batch_size = 32,
+    shuffle=True
+) 
 
 Unet_model = tf.keras.load_model("./")
 
-def preprocess():
-  pass
-  
 def predict():
-  preprocess()
-  Unet_model.predict()
+  Unet_model.predict(data)
+  
   pass
