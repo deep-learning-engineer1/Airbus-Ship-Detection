cebede502a6fe1b13d1c3d4c9d448f08bd0ea75b
deep-learning-engineer1 <148576909+deep-learning-engineer1@users.noreply.github.com>
2024-07-03 12:11:38 +0300

    Update Exploratory_data_exported_from_jupyter_notebook.py

--- Exploratory_data_exported_from_jupyter_notebook.py
+++ Exploratory_data_exported_from_jupyter_notebook.py
@@ -1,3 +1,19 @@
 #Exploring data and sumarazing it
 #for visualization we will take matplotlib
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+number_of_images = 85112
+
+dataset = tensorflow.keras.preprocessing.image_dataset_from_directory(
+    directory = "/kaggle/input/ship-data/my_shipdetection",
+    color_mode = "rgb",
+    batch_size = 32,
+    shuffle=False
+)
+
+plt.plot()
+plt.axis(0, number_of_images)
+plt.show()
+
