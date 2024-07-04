098f15ba11b442afe214a7e29dd0181d3c2879e8
deep-learning-engineer1 <148576909+deep-learning-engineer1@users.noreply.github.com>
2024-07-04 13:17:48 +0300

    Update Exploratory_data_exported_from_jupyter_notebook.py

--- Exploratory_data_exported_from_jupyter_notebook.py
+++ Exploratory_data_exported_from_jupyter_notebook.py
@@ -1,19 +1,42 @@
-#Exploring data and sumarazing it
-#for visualization we will take matplotlib
-import matplotlib.pyplot as plt
+# Exploring data and sumarizing it
+# For visualization we will use matplotlib
+# Our mission is to draw boxes from csv file on images.
+
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image_dataset_from_directory
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
+directory_csv = "/kaggle/input/airbus-ship-detection-data-visualization-css/sample_submission_v2.csv"
+directory_images = "/kaggle/input/ship-data/my_shipdetection"
+new_directory = os.mkdir("/kaggle/working/images")
+
+# We need Computer Vision for drawing box 
+import cv2 as cv
+
+# Images
 number_of_images = 85112
-
-dataset = tensorflow.keras.preprocessing.image_dataset_from_directory(
-    directory = "/kaggle/input/ship-data/my_shipdetection",
+dataset_images = tf.keras.preprocessing.image_dataset_from_directory(
+    directory = directory_images,
     color_mode = "rgb",
     batch_size = 32,
     shuffle=False
 )
 
-plt.plot()
-plt.axis(0, number_of_images)
-plt.show()
+# CSV file
+dataset_csv = pd.read_csv(directory_csv)
+print(dataset_csv)
 
+red = (0, 0, 256)
+
+for i in range(number_of_images):
+    encoded_pixels = dataset_csv["EncodedPixels"][i]
+    if encoded_pixels == "":
+        print("image doesn't have any ship")
+        continue
+    image = dataset_images[i]
+    cv.line(image, encoded_pixels, red)
+print("All images are ready.")
+
+# i need to add annotated images to all images.
